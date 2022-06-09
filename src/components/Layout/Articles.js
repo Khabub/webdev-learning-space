9a4bc27726ae8c4b9a859b38f380db3898417621
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-06-09 22:59:58 +0200

    working on mobile response

--- src/components/Layout/Articles.js
+++ src/components/Layout/Articles.js
@@ -1,19 +1,22 @@
 import React from "react";
 import bgImageArticle from "../../assets/Rectangle_4picture.png";
 import classes from "./Articles.module.css";
+import arrowImg from "../../assets/Vector_3sign.png";
 
 const Articles = () => {
   return (
     <div className={classes.container}>
       <div className={classes.articleBox}>
-        <img
-          src={bgImageArticle}
-          className={classes.bgImgArticle}
-          alt="bg article"
-        ></img>
+        <div className={classes.bgImgArticle}>
+          <img
+            src={bgImageArticle}            
+            alt="bg article"
+          ></img>
+        </div>
         <div className={classes.articleText}>
           <h2>Exploration</h2>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+          <img src={arrowImg} className={classes.arrowImg} alt="arrow"></img>
         </div>
       </div>
     </div>
