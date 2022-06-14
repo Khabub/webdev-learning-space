53cbc33525c2f73b80235ece60319083e73ff558
Robert Rozehnal <rob.kab.roz@gmail.com>
2022-06-14 19:16:45 +0200

    save

--- src/components/Layout/Main.js
+++ src/components/Layout/Main.js
@@ -1,5 +1,5 @@
 import React from "react";
-// import backgroundImage from "../../assets/Vectary_texturebackground.png";
+import backgroundImage from "../../assets/Vectary_texturebackground.png";
 import backgroundImageDesktop from "../../assets/Vectary_texturebg-desktop.png";
 import robotImage from "../../assets/Vectary_texturerobot.png";
 import classes from "./Main.module.css";
@@ -7,16 +7,16 @@ import classes from "./Main.module.css";
 const Main = () => {
   return (
     <div className={classes.container}>
-      {/* <img
+      <img
         src={backgroundImage}
         alt="background"
         className={classes.bgImage}
-      ></img> */}
-      <img
+      ></img>
+      {/* <img
         src={backgroundImageDesktop}
         alt="background"
         className={classes.bgImageDesktop}
-      ></img>
+      ></img> */}
 
       <img src={robotImage} alt="robot" className={classes.robotImage}></img>
     </div>
